876ebe1e8a7c1d63f6970050d72200a602c7cc61
Ibnu Hambali <hambaliibnu27@gmail.com>
2021-11-22 22:29:55 +0700

    feat: spring project - folder

---
+++ si_retail/src/test/java/apap/tk/si_retail/SiRetailApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.tk.si_retail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiRetailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
