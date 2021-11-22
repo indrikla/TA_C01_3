833e52fbcd996caaddf1b42f4166e3390c3dfe8c
Ibnu Hambali <hambaliibnu27@gmail.com>
2021-11-22 22:43:54 +0700

    feat: home - html and controller files

---
+++ si_retail/src/main/java/apap/tk/si_retail/controller/PageController.java
@@ -0,0 +1,14 @@
+package apap.tk.si_retail.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+    @RequestMapping("/")
+    public String home(){
+        return "home";
+    }
+
+    @RequestMapping("/login")
+    public String login(){return "login";}
+}
