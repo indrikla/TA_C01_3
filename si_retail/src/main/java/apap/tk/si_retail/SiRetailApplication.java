b034cd173e796c9e536ca15ac042bd719576cd31
ibnuhambali <53441711+ibnuhambali@users.noreply.github.com>
2021-11-22 22:57:38 +0700

    Ibnuhambali (#2)
    * feat: spring project - folder
    
    * feat: home - html and controller files

---
+++ si_retail/src/main/java/apap/tk/si_retail/SiRetailApplication.java
@@ -0,0 +1,13 @@
+package apap.tk.si_retail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiRetailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiRetailApplication.class, args);
+	}
+
+}
