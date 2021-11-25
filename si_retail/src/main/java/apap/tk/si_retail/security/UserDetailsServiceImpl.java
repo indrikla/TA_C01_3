95d668fe7ea1e097882eac144b956fa8b0031a81
ibnuhambali <53441711+ibnuhambali@users.noreply.github.com>
2021-11-25 18:06:35 +0700

    Login & Logout has been done (#8)
    * feat: spring project - folder
    
    * feat: home - html and controller files
    
    * feat: UserDetailService - Security

--- si_retail/src/main/java/apap/tk/si_retail/security/UserDetailsServiceImpl.java
+++ si_retail/src/main/java/apap/tk/si_retail/security/UserDetailsServiceImpl.java
@@ -1,7 +1,6 @@
-package apap.tk.si_retail.service;
-
+package apap.tk.si_retail.security;
 import apap.tk.si_retail.model.UserModel;
-import apap.tk.si_retail.repository.UserDB;
+import  apap.tk.si_retail.repository.UserDB;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -13,16 +12,14 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
-
 @Service
-public class UserDetailServiceImpl  implements UserDetailsService {
-
+public class UserDetailsServiceImpl implements UserDetailsService{
     @Autowired
     private UserDB userDB;
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserModel user = userDB.findByUsername(username);
+
         Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
         grantedAuthorities.add(new SimpleGrantedAuthority(user.getRole().getNama()));
         return new User(user.getUsername(), user.getPassword(), grantedAuthorities);
