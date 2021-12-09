166aa5ad096cad302f1cfcafb169054bb9828057
fahmidani24 <71580023+fahmidani24@users.noreply.github.com>
2021-12-09 20:35:01 +0700

    Feat: Fitur Menyetujui dan Menolak Permintaan Cabang (#27)
    * feat: Read Cabang Details & Update User
    
    * feat: added table for item in cabang
    
    * Fitur Menolak dan Menyetujui Permintaan Cabang
    
    * Fix: Application-Production For merge

--- si_retail/src/main/java/apap/tk/si_retail/repository/CabangDB.java
+++ si_retail/src/main/java/apap/tk/si_retail/repository/CabangDB.java
@@ -12,4 +12,5 @@ import java.util.Optional;
 public interface CabangDB extends JpaRepository<CabangModel, Long> {
     //    Optional<CabangModel> findByIdCabang(Long idCabang);
     List<CabangModel> findAllByPenanggungJawab(UserModel penanggungJawab);
+    List<CabangModel> findAllByStatus(Integer status);
 }
